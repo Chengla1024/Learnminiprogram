8e2b3088d7c606b9765f462836f17caa87b3bd86
Chengla <2945251335.com>
2020-05-12 20:11:45 +0800

    初始化项目

---
+++ pages/home/home.js
@@ -0,0 +1,78 @@
+// pages/home/home.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  },
+  //监听页面滚动
+  onPageScroll(obj){
+    console.log(obj)
+  },
+  //监听页面滑动了底部
+  onReachBottom(){
+    console.log("页面到达了底部")
+  },
+  //监听下拉刷新
+  onPullDownRefresh(){
+    console.log("下拉刷新")
+  }
+})
